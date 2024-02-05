abfa0a9382abf2098a44080b45e42a51d08c0204
Akshay <naganwarakshay@gmail.com>
2024-02-05 19:37:10 +0530

    second commit

---
+++ src/main/java/com/image/imageData/ImageDataProjectApplication.java
@@ -0,0 +1,13 @@
+package com.image.imageData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImageDataProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImageDataProjectApplication.class, args);
+	}
+
+}
